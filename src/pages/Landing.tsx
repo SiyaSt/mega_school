0e88c09a48aa06f7fcc6c8e2924baffc86c089e9
Анастасия <anastasiya.stecenko@gmail.com>
2026-01-30 09:13:25 +0300

    6

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../components/common/Button";
 import { Card } from "../components/common/Card";
